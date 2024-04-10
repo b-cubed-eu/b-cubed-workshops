c916963a2e7b49b830562d720167e820b62bacab
wlangera <ward.langeraert@inbo.be>
2024-04-10 17:35:02 +0200

    add input layer

--- workshops/02/gcube_workflow.R
+++ workshops/02/gcube_workflow.R
@@ -1,4 +1,5 @@
 ### Set-up
+
 # Install package if necessary
 install <- FALSE
 if (install) {
@@ -10,3 +11,14 @@ if (install) {
 library(gcube)     # simulate biodiversity data cubes
 library(sf)        # work with spatial objects
 library(tidyverse) # data wrangling and visualisation
+
+### Input
+
+# Create a polygon to simulate occurrences
+polygon <- st_polygon(list(cbind(c(5, 10, 10, 6, 2, 1, 5),
+                                 c(2, 1, 7, 10, 9, 5, 2))))
+
+# Visualise
+ggplot() +
+  geom_sf(data = polygon) +
+  theme_minimal()
